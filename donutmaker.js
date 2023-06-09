b77d7a9dba27efaaecb379bfe71baa4005657748
Heladiusz <eladiocolon29@gmail.com>
2023-06-09 18:30:33 -0400

    fixed a few buttons errors

--- donutmaker.js
+++ donutmaker.js
@@ -22,8 +22,9 @@ const openModal = (modalId) => {
 }
 
 const closeModal = (modalId) => {
-  const modal = document.getElementById(`${modalId}Modal`)
- modal.style.display = "none"}
+  const modal = document.getElementById(`${modalId}Modal`);
+  modal.style.display = "none";
+}
 
 let count = 0;
 const countElement = document.getElementById("count");
@@ -34,27 +35,25 @@ const autoClickerElement = document.getElementById("auto-clicker");
 const purchaseAutoClickerButton = document.getElementById("purchase-auto-clicker");
 const activateAutoClickersButton = document.getElementById("activate-auto-clickers");
 const stopAutoClickersButton = document.getElementById("stop-auto-clickers");
-const resetGameButton = document.getElementById("reset-game-button")
+const resetGameButton = document.getElementById("reset-game-button");
 
 let autoClickerCost = 100;
 let autoClickerInterval;
 
-//function to increment donut count
+// function to increment donut count
 const incrementCount = () => {
   count++;
   countElement.textContent = count;
 };
-
-//function to purchase auto clicker
+// function to purchase auto clicker
 const purchaseAutoClicker = () => {
   if (count >= autoClickerCost) {
     count -= autoClickerCost;
     autoClickerCount++;
     countElement.textContent = count;
     autoClickerElement.textContent = autoClickerCount;
-
-    autoClickerCost = math.round(autoClickerCost * 1.1);
-    purchaseAutoClickerButton.textContent = `Purchase Auto Clicker (${autoClickerCost} donuts)`;
+    autoClickerCost = Math.round(autoClickerCost * 1.1);
+    purchaseAutoClickerButton.textContent = `Purchase Auto Clicker(${autoClickerCost} donuts)`;
   }
 };
 
@@ -63,26 +62,22 @@ const activateAutoClickers = () => {
     count += autoClickerCount;
     countElement.textContent = count;
   }, 1000);
-
-  //stop the auto clickers after a certain period
-  setTimeout(() => {
-    stopAutoClickers();
-  }, 1000);
 };
 
-//function to stop auto clickers
+// function to stop auto clickers
 const stopAutoClickers = () => {
   clearInterval(autoClickerInterval);
+  autoClickerInterval = null;
 };
 
-//add event listeners
+// add event listeners
 donutButton.addEventListener("click", incrementCount);
 purchaseAutoClickerButton.addEventListener("click", purchaseAutoClicker);
 activateAutoClickersButton.addEventListener("click", activateAutoClickers);
 stopAutoClickersButton.addEventListener("click", stopAutoClickers);
-resetButton.addEventListener('click', resetgame)
 
-//resets the game - reload page
-const resetgame = () => {
+// resets the game - reload page
+const resetGame = () => {
   location.reload();
-}
+};
+resetGameButton.addEventListener("click", resetGame);
